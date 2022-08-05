5c8e1056b242cbad4a5eb98077924bbb5296be32
James Reed <james@twiddlingbits.net>
2022-08-05 12:15:05 -0400

    debouncer: Use GLib timeouts

--- lightend/debouncer.py
+++ lightend/debouncer.py
@@ -1,35 +1,12 @@
-import time
-from threading import Lock, Thread
+from gi.repository import GLib
 
 
 class Debouncer:
     def __init__(self, interval=3):
-        self.mutex = Lock()
-        self.update_time = None
-        self.running = False
-        self.interval = interval
-
-    def _target(self, callback):
-        while True:
-            self.mutex.acquire()
-            try:
-                t = time.time() - self.update_time
-                if t >= self.interval:
-                    self.running = False
-            finally:
-                self.mutex.release()
-            if not self.running:
-                callback()
-                return
-            time.sleep(self.interval)
+        self.interval = interval * 1000
+        self.source = None
 
     def start(self, callback):
-        self.mutex.acquire()
-        try:
-            self.update_time = time.time()
-            if self.running:
-                return
-            Thread(target=self._target, args=(callback,), daemon=True).start()
-            self.running = True
-        finally:
-            self.mutex.release()
+        if self.source:
+            GLib.source_remove(self.source)
+        self.source = GLib.timeout_add(self.interval, callback)
